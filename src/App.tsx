8f71569d78e0a397caea0bcfbe1dc88f107ba0c0
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2024-02-17 20:38:26 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="text-6xl text-blue-700 text-center font-extrabold">
+        React App
+      </h1>
+    </>
+  );
+};
+
+export default App;
